3891c6c959122e9d577e3fcc80a7666d1379c0cc
Pheonix21 <bryan.marquez76@gmail.com>
2016-02-06 21:37:27 -0700

    Create main.cpp
    Main cpp file that will run the Hello World program.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h> 
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello World!" << endl;
+	system("PAUSE");
+	return 0;
+}
